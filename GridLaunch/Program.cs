9e4dafa439a8a27deee290d4458b124b4a1a9aa9
Daniel Gillespie <dan@danielgillespie.net>
2018-06-01 14:36:14 -0700

    inital commit of working project

---
+++ GridLaunch/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using System.Runtime.InteropServices;
+
+namespace GridLaunch
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        private const string EXECUTABLE_JAR = "youtubegrid.jar";
+
+        private static string getScreensaverPath()
+        {
+            string result = null;
+
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\YouTubeGrid"))
+                {
+                    if (key != null)
+                    {
+                        result = (String)key.GetValue("SCREEN_SAVER_PATH");
+                    }
+                }
+            }
+            catch (Exception ex)  //just for demonstration...it's always best to handle specific exceptions
+            {
+                //react appropriately
+            }
+
+            return result; 
+        }
+
+        private static string getJavaInstallationPath()
+        {
+            string environmentPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+            if (!string.IsNullOrEmpty(environmentPath))
+            {
+                return environmentPath;
+            }
+
+            string javaKey = "SOFTWARE\\JavaSoft\\Java Runtime Environment\\";
+            using (Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(javaKey))
+            {
+                string currentVersion = rk.GetValue("CurrentVersion").ToString();
+                using (Microsoft.Win32.RegistryKey key = rk.OpenSubKey(currentVersion))
+                {
+                    return key.GetValue("JavaHome").ToString();
+                }
+            }
+        }
+
+        private static void launchScreensaver(String appPath)
+        {
+            string javaBin = getJavaInstallationPath() + "\\bin\\java.exe";
+            string jarPath = appPath + "\\" + EXECUTABLE_JAR;
+
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "java";
+            proc.StartInfo.Arguments = "-jar " + jarPath + " /s";
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static void hideConsole()
+        {
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+        }
+
+        static void Main(string[] args)
+        {
+            hideConsole();
+
+            string appPath = getScreensaverPath();
+            if(!String.IsNullOrEmpty(appPath))
+            {
+                launchScreensaver(appPath);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Grid Launch
+
+Grid Launch is a Windows JAR launcher made for the ![YouTubeGrid Screensaver](https://github.com/dantheman213/youtubegrid-screensaver).
+
+## Getting Started
+
+This project is an active work in progress. TBD!
